8084ceb9d3c85fc7a324f728a654b1049b7008c7
Halima Muktar <halimamuk1307@gmail.com>
2025-08-20 16:27:06 +0300

    manifest.json

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,8 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Link to manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* PWA theme color */}
+        <meta name="theme-color" content="#FFFFFF" />
+        {/* Icons for different devices */}
+        <link rel="icon" href="/icons/android-chrome-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
